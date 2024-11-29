33f7917f82641b12e22b3dc9b33a87da88b4dc7b
Vchslv-Zkhrv <vchslv.zkhrv@gmail.com>
2024-11-29 20:44:07 +0300

    tests

--- tests/test_split.py
+++ tests/test_split.py
@@ -14,18 +14,13 @@ def test_zero_max_len():
             pass
 
 
-# def test_just_text():
-#     text = 'a'*1024
-#     chunks = list(c for c in split_msg(text, 128))
-#     assert len(chunks) == 8
+def test_just_text():
+    text = 'a'*1024
+    chunks = list(c for c in split_msg(text, 128))
+    assert len(chunks) == 8
 
 
 def test_splittable():
     text = ' <div><strong><i>' + 'a'*1024 + '</i></strong></div>'
     chunks = list(c for c in split_msg(text, 128))
     assert len(chunks) > 8
-
-def test_unsplittable():
-    text = '<a href=""><div><b>' + 'a'*1024 + '</b></div></a>'
-    with pytest.raises(ValueError):
-        list(c for c in split_msg(text, 128))
